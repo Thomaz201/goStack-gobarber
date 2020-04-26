8eb0729571d5b28cb18605aee38e7f93242cd244
Lucas Thomaz Ribeiro <lucasthomazr@hotmail.com>
2020-04-26 13:43:50 -0300

    Validação de sessões (incompleto)

---
+++ src/services/CreateSessionsService.ts
@@ -0,0 +1,10 @@
+interface Request {
+  email: string;
+  password: string;
+}
+
+class CreateSessionsService {
+  public async execute({ email, password }: Request): Promise<void> {
+    const;
+  }
+}
