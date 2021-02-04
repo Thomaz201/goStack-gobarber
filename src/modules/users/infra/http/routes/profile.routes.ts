3803eeb4ed6538b2910eecae74304a28e0f10165
Lucas Thomaz <lucasthomazr@hotmail.com>
2021-02-04 00:27:58 -0300

    Routes values validation with celebrate

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import ProfileController from '../controllers/ProfileController';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthentication from '@modules/users/infra/http/middlewares/ensureAuthentication';
 
@@ -9,6 +10,24 @@ const profileController = new ProfileController();
 profileRouter.use(ensureAuthentication);
 
 profileRouter.get('/', profileController.show);
-profileRouter.put('/', profileController.update);
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.when('old_password', {
+        is: Joi.exist(),
+        then: Joi.required(),
+      }),
+      password_confirmation: Joi.when('password', {
+        is: Joi.exist(),
+        then: Joi.valid(Joi.ref('password')).required(),
+      }),
+    },
+  }),
+  profileController.update,
+);
 
 export default profileRouter;
